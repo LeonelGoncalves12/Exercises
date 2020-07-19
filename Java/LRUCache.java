8b229acb982ce33493c188d297f1a07fa2bc411f
LeonelGoncalves12 <pg33003@alunos.uminho.pt>
2020-07-19 02:29:38 +0100

    Exercises - 19/07/2020

---
+++ Java/LRUCache.java
@@ -0,0 +1,80 @@
+/*
+LeetCode - 146. LRU Cache
+
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache( 2 );
+
+    cache.put(1, 1);
+    cache.put(2, 2);
+    cache.get(1);       // returns 1
+    cache.put(3, 3);    // evicts key 2
+    cache.get(2);       // returns -1 (not found)
+    cache.put(4, 4);    // evicts key 1
+    cache.get(1);       // returns -1 (not found)
+    cache.get(3);       // returns 3
+    cache.get(4);       // returns 4
+*/
+
+import java.util.*;
+
+class LRUCache {
+    private Map<Integer, Integer> cache;
+    private static int CAPACITY;
+
+    public LRUCache(int capacity) {
+        CAPACITY = capacity;
+        this.cache = new LinkedHashMap<Integer, Integer>();
+    }
+
+    public int get(int key) {
+        if (hasEqual(key)) {
+            int value = this.cache.get(key);
+            updateMap(key, value);
+            return this.cache.get(key);
+        } else {
+            return -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        if (hasSpace() && !hasEqual(key)) {
+            this.cache.put(key, value);
+        } else if (hasEqual(key)) {
+            updateMap(key, value);
+        } else {
+            int removeKey = this.cache.entrySet().iterator().next().getKey();
+            this.cache.remove(removeKey);
+            this.cache.put(key, value);
+        }
+    }
+
+    public boolean hasSpace() {
+        return this.cache.size() < CAPACITY;
+    }
+
+    public boolean hasEqual(int key) {
+        return this.cache.containsKey(key);
+    }
+
+    public void updateMap(int key, int value) {
+        this.cache.remove(key);
+        this.cache.put(key, value);
+    }
+}
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
